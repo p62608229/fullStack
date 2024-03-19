f49eeccaee5c5c0c6fb774d2bcbfd49392b1916c
Tzipora Aviezri <101580058+TziporaAviezri@users.noreply.github.com>
2024-03-19 11:52:01 +0000

    add commrnt style

--- src/components/comments/addComment.jsx
+++ src/components/comments/addComment.jsx
@@ -1,7 +1,9 @@
 
 
-import { useState } from 'react';
+import { Form, Field } from 'react-final-form';
 import { useDispatch, useSelector } from 'react-redux';
+import { classNames } from 'primereact/utils';
+import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
 import { AddComment } from '../../Redux/API/comment';
 import { InputTextarea } from 'primereact/inputtextarea';
@@ -11,14 +13,24 @@ export const Addcomment = (props) => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const currentUser = useSelector(state => state.users.currentUser);
-  console.log(currentUser, "current user from add comments")
-  const [commentv, setCommentv] = useState();
 
-  const handleSubmit = () => {
+  const currentUser = useSelector(state => state.users.currentUser);
+
+  const validate = (data) => {
+    let errors = {};
+
+    if (!data.commentv) {
+      errors.commentv = 'required provider comment.';
+    }
+
+    return errors;
+
+};
+
+  const onSubmit = (data) => {
     const newComment = {
       namecomment: currentUser.name,
-      ContentCommentv: commentv,
+      ContentCommentv: data.commentv,
       CommentUserId: currentUser.id,
     }
     dispatch(AddComment(newComment))
@@ -27,12 +39,32 @@ export const Addcomment = (props) => {
     else navigate("/")
   };
 
+  const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
+  const getFormErrorMessage = (meta) => {
+      return isFormFieldValid(meta) && <small className="p-error">{meta.error}</small>;
+  };
+
   return (
 
-    <div  style={{border: "10px lightgray"}}>
-      <label>Please add your comment:  </label> <br />
-      <InputTextarea value={commentv} onChange={(e) => setCommentv(e.target.value)} rows={5} cols={30} /><br />
-      <button onClick={handleSubmit} className="mt-2">הוסף</button>
+    <div className="form-demo" style={{ padding: "30px" }}>
+      <div className="flex justify-content-center">
+          <div className="card">
+              <Form onSubmit={onSubmit} initialValues={{ commentv: '' }} validate={validate} render={({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit} className="p-fluid">
+                      <Field name="commentv" render={({ input, meta }) => (
+                          <div className="field">
+                              <span className="p-float-label">
+                                  <InputTextarea id="commentv" {...input}  autoFocus className={classNames({ 'p-invalid': isFormFieldValid(meta) })}  rows={5} cols={30} />
+                                  <label htmlFor="commentv" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Please add your comment:</label>
+                              </span>
+                              {getFormErrorMessage(meta)}
+                          </div>
+                      )} />
+                      <Button type="submit" label="Submit" className="mt-2" />
+                  </form>
+              )} />
+          </div>
+      </div>
     </div>
   );
 };
