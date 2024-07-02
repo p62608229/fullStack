8e6069ce101a8032c3c4427ac902b27a36b8cbc2
sara-shpigel <sara671876@gmail.com>
2024-07-02 12:27:52 +0300

    unscroll - sara

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
+import '../css/home.css';
 
 const Footer = () => {
-  const footerStyle = {
-    backgroundColor: '#443f3f',
-    color: 'white',
-    padding: '20px',
-    textAlign: 'center',
-    // position: 'fixed', // Set position to fixed
-    bottom: 0, // Align to the bottom of the viewport
-    left: 0, // Align to the left edge of the viewport
-    width: '100%', // Occupy full width
-  };
-
   return (
-    <footer style={footerStyle}>
+    <footer className="section" style={{ backgroundColor: '#443f3f', color: 'white' }}>
       We are not responsible for content uploaded to the Helpbook by users. Employers are responsible for hiring as required. <br />
       © 2011-2024 All rights reserved to Kasliva Ltd.
     </footer>
