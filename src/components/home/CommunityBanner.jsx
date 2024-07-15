fa3ceff8a534cda6991d05f913931ab346131c76
sara-shpigel <sara671876@gmail.com>
2024-07-15 11:07:03 +0300

    image, logo and about

--- src/components/home/CommunityBanner.jsx
+++ src/components/home/CommunityBanner.jsx
@@ -3,7 +3,7 @@ import '../../css/home.css';
 
 const CommunityBanner = () => {
   return (
-    <div className="community-banner" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/community.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="community-banner" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/img/community.jpg)`, backgroundSize: 'contain', backgroundPosition: 'center' }}>
       <div className="community-banner-title">Time to find good people!</div>
       <div className="community-banner-subtitle">A smart job board for the home and family</div>
     </div>
